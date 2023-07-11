67583b74d3539f1cebab99cc622f62626c90d28a
duc <duc_n44@yahoo.com>
2023-07-10 20:45:15 -0600

    added firstName and lastName

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -72,6 +72,8 @@ router.post(
       if (user) {
         const safeUser = {
           id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
           email: user.email,
           username: user.username,
         };
